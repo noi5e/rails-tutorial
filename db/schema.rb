874b476d908d726977fc799efde2213176d29794
noi5e <will.gutierrez@gmail.com>
2026-03-02 14:37:56 -0800

    scaffold Micropost resource and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_27_020240) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_02_223629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
